7f44010c6e4d7f59307cb646d70fec3bf2c251fc
cm-engineer-taro <tsukamoto.taro@classmethod.jp>
2023-09-18 23:17:58 +0900

    feat: スナップショットテストを追加

--- iac/lib/iac-stack.ts
+++ iac/lib/iac-stack.ts
@@ -1,16 +1,16 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { CloudFrontToS3 } from "@aws-solutions-constructs/aws-cloudfront-s3";
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class IacStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'IacQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const cloudFrontAndS3 = new CloudFrontToS3(this, "CloudFrontToS3", {
+      bucketProps: {
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+      },
+    });
   }
 }
--- iac/test/iac.test.ts
+++ iac/test/iac.test.ts
@@ -1,17 +1,11 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Iac from '../lib/iac-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import * as Iac from "../lib/iac-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/iac-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Iac.IacStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test("Snap shot testing", () => {
+  const app = new cdk.App();
+  const stack = new Iac.IacStack(app, "MyTestStack");
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  expect(template.toJSON()).toMatchSnapshot();
 });
