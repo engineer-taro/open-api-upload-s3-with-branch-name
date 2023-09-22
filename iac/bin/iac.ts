067059cf92df193c040051e3e39d697b5cbbfe4f
cm-engineer-taro <tsukamoto.taro@classmethod.jp>
2023-09-22 22:09:00 +0900

    parameter.tsの値を使うように修正

--- iac/bin/iac.ts
+++ iac/bin/iac.ts
@@ -2,9 +2,10 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { FrontStack } from "../lib/front-stack";
+import { devParameter } from "../parameter";
 
 const app = new cdk.App();
 
 new FrontStack(app, "IacStack", {
-  gitHubOrg: "engineer-taro",
+  gitHubOrg: devParameter.gitHubOrg,
 });
